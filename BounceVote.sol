ced54d29b2b300ddae86fb5eb63e4a8c7a932deb
Bevanz <alex.bevans@gmail.com>
2025-03-07 20:23:17 -0800

    update
    update

---
+++ BounceVote.sol
@@ -0,0 +1,98 @@
+// SPDX-License-Identifier: MIT
+
+// ________  __      __.____       _____   
+// \_____  \/  \    /  \    |     /  _  \  
+//  /  / \  \   \/\/   /    |    /  /_\  \ 
+// /   \_/.  \        /|    |___/    |    \
+// \_____\ \_/\__/\  / |_______ \____|__  /
+//        \__>     \/          \/       \/ 
+
+
+pragma solidity ^0.8.0;
+
+interface IQWLAToken {
+    function balanceOf(address owner) external view returns (uint);
+}
+
+contract BounceVote {
+    IQWLAToken public qawlaToken;
+    address public owner;
+    mapping(address => bool) public whitelist;
+    mapping(uint => mapping(address => bool)) public hasVoted;
+    mapping(uint => Poll) public polls;
+    uint public pollCount;
+
+    struct Poll {
+        uint id;
+        string name;
+        address creator;
+        string[5] options; // Array to hold up to 5 options
+        uint optionCount;  // Current count of options
+        uint minQWLAHeld;
+        uint voteCount;
+        bool isOpen;
+        mapping(uint => uint) votes; // Mapping from option index to vote count
+    }
+
+    event PollCreated(uint pollId, address creator, string name);
+    event OptionAdded(uint pollId, string option);
+    event Voted(uint pollId, address voter, uint option);
+    event PollClosed(uint pollId, uint result);
+
+    constructor(address tokenAddress) {
+        qawlaToken = IQWLAToken(tokenAddress);
+        owner = msg.sender;
+        whitelist[owner] = true;
+    }
+
+    modifier onlyWhitelisted() {
+        require(whitelist[msg.sender], "Not whitelisted");
+        _;
+    }
+
+    function addToWhitelist(address user) public {
+        require(msg.sender == owner, "Only owner can add to whitelist");
+        whitelist[user] = true;
+    }
+
+    function createPoll(string memory name, string[5] memory options, uint optionCount, uint minQWLAHeld) public onlyWhitelisted returns (uint) {
+        require(optionCount >= 1 && optionCount <= 5, "Option count must be between 1 and 5");
+        uint pollId = pollCount++;
+        Poll storage newPoll = polls[pollId];
+        newPoll.id = pollId;
+        newPoll.name = name;
+        newPoll.creator = msg.sender;
+        newPoll.minQWLAHeld = minQWLAHeld > 0 ? minQWLAHeld : 1;
+        newPoll.isOpen = true;
+        newPoll.optionCount = optionCount;
+        for (uint i = 0; i < optionCount; i++) {
+            newPoll.options[i] = options[i];
+        }
+        emit PollCreated(pollId, msg.sender, name);
+        return pollId;
+    }
+
+    function addOption(uint pollId, string memory option) public {
+        require(msg.sender == polls[pollId].creator, "Only the creator can add options");
+        require(polls[pollId].optionCount < 5, "Cannot add more than 5 options");
+        polls[pollId].options[polls[pollId].optionCount++] = option;
+        emit OptionAdded(pollId, option);
+    }
+
+    function vote(uint pollId, uint option) public {
+        require(polls[pollId].isOpen, "Poll is closed");
+        require(!hasVoted[pollId][msg.sender], "Already voted");
+        require(qawlaToken.balanceOf(msg.sender) >= polls[pollId].minQWLAHeld, "Not enough QWLA tokens");
+        require(option < polls[pollId].optionCount, "Invalid option");
+        hasVoted[pollId][msg.sender] = true;
+        polls[pollId].votes[option]++;
+        polls[pollId].voteCount++;
+        emit Voted(pollId, msg.sender, option);
+    }
+
+    function closePoll(uint pollId) public {
+        require(msg.sender == polls[pollId].creator || whitelist[msg.sender], "Unauthorized");
+        polls[pollId].isOpen = false;
+        emit PollClosed(pollId, polls[pollId].voteCount);
+    }
+}
